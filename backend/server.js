d9e16fbfcf94a3fa99072a28c37b64643111fee9
MarcoMendiola <mcmendiola@paterostechnologicalcollege.edu.ph>
2025-03-24 20:42:28 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/students", studentRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
